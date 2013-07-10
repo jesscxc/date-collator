7d17c2303858f0771f553b3e3bb197439543d595
Jesse Cummins <me@jessecummins.com>
2013-07-10 14:42:32 -0400

    Add inspiration comment and related working example to main script

--- date_collator.lua
+++ date_collator.lua
@@ -4,6 +4,9 @@
 2013-07
 Jesse Cummins
 https://github.com/jessc
+# With inspiration from evitiello:
+https://github.com/evitiello/TrunkNotesScripts
+
 
 # bug list:
 # todo:
@@ -13,6 +16,30 @@ https://github.com/jessc
 
 
 
+--
+-- Displays the last 20 pages that have been modified.
+--
+function string.starts(String,Start)
+   return string.sub(String, 1, string.len(Start)) == Start
+end
+
+returnString = ""
+
+pageTimes = {}
+pageTimeMapping = {}
+
+for pageIteration, pageTitle in ipairs(wiki.titles()) do
+	pageUpdatedTime = wiki.get(pageTitle).updated
+	table.insert(pageTimes, pageUpdatedTime)
+	pageTimeMapping[pageUpdatedTime] = pageTitle
+end
+table.sort(pageTimes, function(a,b) return a>b end)
+
+for i=1, 20 do
+	returnString = returnString .. "[[" .. pageTimeMapping[pageTimes[i]] .. "]]\n"
+end
+
+return returnString
 
 
 
