da857d632cd7aea1314d1093f384d7faf2d84686
Jesse Cummins <me@jessecummins.com>
2013-07-13 07:48:53 -0400

    Figure out cause for strange string.find bug (hyphen needs escaping with %); remove old comments

--- date_collator.lua
+++ date_collator.lua
@@ -11,7 +11,6 @@ https://github.com/evitiello/TrunkNotesScripts
 -- {{lua try.lua, try_journal, 2, 2009, 2010}}
 
 
-
 --[[
 journal_name = wiki.get(args[1])
 text = journal_name.contents
@@ -24,7 +23,6 @@ num_years = args[2]
 -- month_day = os.date('-%m-%d - ')
 month_day = "-02-19 - "
 
-
 for i = 1, num_years do
 	table.insert(full_dates, args[i+2] .. month_day)
 end
@@ -33,35 +31,18 @@ for i = 1, num_years do
 	returnString = returnString .. full_dates[i] .. "\n"
 end
 
--- For some reason string.find is only working when:
-start, fin = string.find(text, "2010-0")
--- but not when the below. what the heck?
--- start, fin = string.find(text, "\n2010-02")
-
-
 returnString = start .. ", " .. fin .. "\n"
 return returnString
 
 ]]
 
--- text = "2009-02-19 - First day of reading Proust and the Squid\n2010-02-19 - Did website work for clients"
-text = "2010-02-19 - Did website work for clients"
+-- example
+text = "2009-02-19 - First day of reading Proust and the Squid\n2010-02-19 - Did website work for clients"
 
--- This works:
-start, fin = string.find(text, "2010-0")
-
--- This:
--- 2012-0
--- Returns this:
--- 1, 4
--- Why not return:
--- 1, 6?
--- Perhaps has to do with not interpreting "2010-0" as string?
--- And instead is subtracting or something?
-
--- This doesn't:
--- start, fin = string.find(text, "2010-02")
+start, fin = string.find(text, "2010%-02%-19")
 
 returnString = start .. ", " .. fin .. "\n"
 return returnString
+
+
 
