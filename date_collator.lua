88f0df3af187b90de287ee4cc608990857b88cdc
Jesse Cummins <me@jessecummins.com>
2013-07-17 14:30:04 -0400

    Remove old unnecessary code

--- date_collator.lua
+++ date_collator.lua
@@ -15,7 +15,6 @@ https://github.com/evitiello/TrunkNotesScripts
 -- journal = wiki.get(args[1]).contents
 -- start_year = args[2]
 -- end_year = os.date("%Y")
--- journal = "2009-01-14 - Helped wheelchair man up hill\n2009-02-19 - First day of reading Proust and the Squid\n2010-02-19 - Did website work for clients\n2011-08-17 - Read through Genghis Khan book, worked on speech"
 journal = "2008-08-17 - Got Stratocaster\n2009-01-14 - Helped wheelchair man up hill\n2009-02-19 - First day of reading Proust and the Squid\n2010-02-19 - Did website work for clients\n2011-08-17 - Read through Genghis Khan book, worked on speech\n2012-01-14 - While flying in a plane, the pilot dropped the altitude and we went weightless for a second :)\n2012-02-19 - Talked with consultant about business help\n"
 start_year = "2005"
 end_year = "2012"
@@ -28,15 +27,13 @@ returnString = ""
 full_dates = {}
 day_text = ""
 
-
 for year = start_year, end_year do
 	date = year .. month_day
 	table.insert(full_dates, date)
 end
 
-
 -- This appears to be the only way to accurately get
--- the size of a table in Lua, if it might be empty.
+-- the size of a table in Lua, if it might be nil.
 size = 0
 for k, v in pairs(full_dates) do
     size = size + 1
@@ -50,29 +47,13 @@ for i = 1, size do
 	if not (start == nil) then
 		d_start, d_fin = string.find(journal, "\n", start)
 		if not (d_start == nil) then
-			-- day_text = day_text .. start .. ", " .. d_fin .. "\n"
-			-- Something like:
-			-- day_text = day_text .. (string.get journal d_start d_fin) .. "\n"
-			-- returnString = returnString .. day_text .. "\n\n"
-
 			returnString = returnString .. string.sub(journal, start, d_fin)
-		-- else
 		end
 	else
-
-		-- returnString = returnString .. day_text .. "\n\n"
-		-- returnString = returnString .. start .. ", " .. fin .. "\n"
 	end
-
-
-	-- Something like, if find date in journal,
-	-- add start and fin to returnString, otherwise add nothing.
-	-- Then set day_text = to string.find from start to the next "\n".
-	-- Join all these day_texts with "\n" and return them.
 end
 
 return returnString
--- return day_text
 
 
 
