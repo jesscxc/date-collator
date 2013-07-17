2f70c456f92125457bb64ff36c0ea0a36b48688f
Jesse Cummins <me@jessecummins.com>
2013-07-17 14:58:16 -0400

    Change date_collator.lua from preset date variables to use the os.date function to get the current day

--- date_collator.lua
+++ date_collator.lua
@@ -12,17 +12,17 @@ https://github.com/evitiello/TrunkNotesScripts
 -- {{lua try.lua, try_journal, 2008}}
 
 
--- journal = wiki.get(args[1]).contents
+journal = wiki.get(args[1]).contents
 -- start_year = args[2]
 -- end_year = os.date("%Y")
-journal = "2008-08-17 - Got Stratocaster\n2009-01-14 - Helped wheelchair man up hill\n2009-02-19 - First day of reading Proust and the Squid\n2010-02-19 - Did website work for clients\n2011-08-17 - Read through Genghis Khan book, worked on speech\n2012-01-14 - While flying in a plane, the pilot dropped the altitude and we went weightless for a second :)\n2012-02-19 - Talked with consultant about business help\n"
+-- journal = "2008-08-17 - Got Stratocaster\n2009-01-14 - Helped wheelchair man up hill\n2009-02-19 - First day of reading Proust and the Squid\n2010-02-19 - Did website work for clients\n2011-08-17 - Read through Genghis Khan book, worked on speech\n2012-01-14 - While flying in a plane, the pilot dropped the altitude and we went weightless for a second :)\n2012-02-19 - Talked with consultant about business help\n"
 start_year = "2005"
 end_year = "2012"
 
 -- To make this work with the provided journal,
 -- going to manually set month_day for now. Otherwise use:
--- month_day = os.date("-%m-%d - ")
-month_day = "-02-19 - "
+month_day = os.date("-%m-%d - ")
+-- month_day = "-02-19 - "
 returnString = ""
 full_dates = {}
 day_text = ""
