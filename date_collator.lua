c673552d3b550936decab47bc4b953aa939ddf41
Jesse Cummins <me@jessecummins.com>
2013-07-18 17:36:26 -0400

    Change end_year to use this year's date rather than a magic variable

--- date_collator.lua
+++ date_collator.lua
@@ -9,16 +9,15 @@ https://github.com/evitiello/TrunkNotesScripts
 # todo:
 ]]
 
--- {{lua try.lua, try_journal, 2008}}
+-- {{lua date_collator.lua, journal, 2000}}
 
 
 journal = wiki.get(args[1]).contents
+
 start_year = args[2]
-
--- end_year = os.date("%Y")
-end_year = "2012"
-
+end_year = os.date("%Y")
 month_day = os.date("-%m-%d - ")
+
 returnString = ""
 full_dates = {}
 day_text = ""
@@ -28,12 +27,14 @@ for year = start_year, end_year do
 	table.insert(full_dates, date)
 end
 
+
 -- This appears to be the only way to accurately get
 -- the size of a table in Lua, if it might be nil.
 size = 0
 for k, v in pairs(full_dates) do
     size = size + 1
 end
+
 
 for i = 1, size do
 	date = full_dates[i]
@@ -49,7 +50,7 @@ for i = 1, size do
 	end
 end
 
-return returnString
 
+return returnString
 
 
