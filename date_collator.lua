09e9359ccaab7ee36f247eab15a3134aa6cb374a
Jesse Cummins <me@jessecummins.com>
2013-07-19 04:44:37 -0400

    Clean up script; add clarifying comment

--- date_collator.lua
+++ date_collator.lua
@@ -1,26 +1,21 @@
--- date_collator.lua
 --[[
 2013-07
 Jesse Cummins
 https://github.com/jessc
 # With inspiration from evitiello:
 https://github.com/evitiello/TrunkNotesScripts
-# bug list:
-# todo:
 ]]
 
+-- Example line to place in Trunk Notes
 -- {{lua date_collator.lua, journal, 2000}}
-
-
 journal = wiki.get(args[1]).contents
-
 start_year = args[2]
+
 end_year = os.date("%Y")
 month_day = os.date("-%m-%d - ")
 
 returnString = ""
 full_dates = {}
-day_text = ""
 
 for year = start_year, end_year do
 	date = year .. month_day
@@ -38,6 +33,8 @@ end
 
 for i = 1, size do
 	date = full_dates[i]
+	-- Have to do some funky escaping
+	-- because Lua sees hyphens as special
 	date = string.gsub(date, "-", "%%-")
 
 	start, fin = string.find(journal, date)
@@ -46,7 +43,6 @@ for i = 1, size do
 		if not (d_start == nil) then
 			returnString = returnString .. string.sub(journal, start, d_fin)
 		end
-	else
 	end
 end
 
